4d1cfda6db502da04d6f7f9262e71865d89d1f50
evleria <valeryia.vidzevich@gmail.com>
2021-09-16 00:23:07 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gh/evleria/aws/terraform-aws-poc
+
+go 1.16
---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+# SECURITY GROUP
+resource "aws_security_group" "ecs_tasks_sg" {
+  name   = "ecs_tasks_sg"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  security_group_id = aws_security_group.ecs_tasks_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ingress_through_http" {
+  security_group_id = aws_security_group.ecs_tasks_sg.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# ECS TASK EXECUTION IAM ROLE
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
